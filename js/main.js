0f0fb74bdbedd01c8c97890be07e2f87862f0ea8
FrancescoCapriolo <francesco.capriolo92@gmail.com>
2022-03-08 22:07:12 +0100

    completed exercice

--- js/main.js
+++ js/main.js
@@ -1,187 +1,148 @@
-//funzione per creare una nuova griglia
-function createGame() {
-    //prima bisogna resettare ogni volta che si crea una nuova griglia
-    document.querySelector("#grid").innerHTML = "";
-    //recupero il livello selezionato
-    const level = parseInt(document.getElementById("input-level").value);
-    // prima di generare le varie celle devo assegnare una variabile
-    // da poter dare le varie gradezze in maniera omogenea
-    let cellsNumber = 0;
-    let cellsRow = 0;
-    //generare i vari tipi di livello con lo switch
-    switch (level) {
-        case 1:
-            cellsNumber = 100;
-            //^ oppure fare cellsRow=10;
-            break;
-        case 2:
-            cellsNumber = 81;
-            //^ oppure fare cellsRow=9;
-            break;
-        case 3:
-            cellsNumber = 49;
-            //^ oppure fare cellsRow=7;
-            break;
-    }
-    //$ sqrt significa la radice quadrata di cellsNumber
-    cellsRow = Math.sqrt(cellsNumber);
-
-    //genera le bombe casuali
-    const bombs = generateBomb(16, cellsNumber);
-    console.warn(bombs);
-
-    //ciclo per in vari numeri delle celle
-    for (let i = 1; i <= cellsNumber; i++) {
-        const gridSquare = createSquare(i, cellsRow);
-        //aggiungo un figlio lla griglia grande
-        document.querySelector("#grid").appendChild(gridSquare);
-    }
-
+//$ funzione che genera il singolo quadrato
+function createSquare(number, cellsPerRow) {
+    let cell = document.createElement('div');
+    // cell.className = ('square');
+    cell.classList.add('box');
+    cell.style.width = `calc(100% / ${cellsPerRow})`
+    cell.style.height = cell.style.width;
+    cell.innerHTML = number;
+    //ritorno del valore chiamato cella
+    return cell;
 }
 
-//function che restituisce un div 
-function createSquare(i, cellsRow) {
-    //creo un elemento
-    const currentGrid = document.createElement("div");
-    //aggiungo il css box
-    currentGrid.classList.add("box");
-    //aggiungo lo stile e mi calcolo la width
-    currentGrid.style.width = `calc(100% / ${cellsRow})`;
-    //la height è uguale alla width
-    currentGrid.style.height = currentGrid.style.width;
-    //inserisco il numero della cella
-    currentGrid.innerHTML += [i];
-    //appena premo un quadrato della griglia gli do un colore
-    currentGrid.addEventListener('click', function () {
-        if (bombs.includes(i)) {
-            this.classList.add("box-bomber");
-
-            writeInElementById('points', `mi dispiace il tuo punteggio è: ${points}`)
-        } else {
-            this.classList.add('box-clicked');
-            points++;
-            writeInElementById("points", ` il tuo punteggio è: ${points}`)
-        }
-    })
-    // restituisce il valore
-    return currentGrid;
-}
-
-/* //funzione per randomicizzare le bombe 
-
-function randomInteger(min, max) {
-    if (isNaN.parseInt(min) || isNaN.parseInt(max)) {
-        console.error("randominte(min,max) ha bisogno di due numeri per argomento");
-    }
-    return (Math.floor(Math.random() * ((max + 1) - min) + max));
-}
- */
-
-
-//funzione per generare in modo non ripetitivo le bombe
-
-function generateRandomUnique(blacklist, min, max) {
-    //crezione della variabile che controlla il numero valido
-    let check = true;
-    let randomInt;
-    //il ciclo continua finchè trova un numero valido
-    while (check) {
-        //genero un numero
-        randomInt = (Math.floor(Math.random() * ((max + 1) - min) + min));
-        //se il numero è presente nella lista
-        if (blacklist.includes(randomInt)) {
-            //se trovo un numero presente esco dal ciclo while
-            check = false;
-        }
-    }
-    return randomInt;
-}
-
-//funziona che genera le 16 bombe
-function generateBomb(bombs, cellsNumber) {
-    //creo un array per contenere le bombe
+//$funzione per creare bombe nelle celle
+function generateBombList(bombs, numberOfCells) {
+    // dati il numero di bombe da generare "bombs"
+    // ed il numero di celle disponibili (generate) "numberOfCells"
+    // inizializzo a vuoto un nuovo array
     const bombList = [];
-    //per ogni bomba ne genero una nuova in un'altra cella
-    for (let i = 0; i < bombs; i++) {
-        //aggiungo la funzione che crea le bombe
-        bombList.push(generateRandomUnique(bombList, 1, cellsNumber));
+    // per ogni bomba
+    for (i = 0; i < bombs; i++) {
+        // ne genero una nuova che non sia già presente e la aggiungo alla lista delle bombe
+        bombList.push(generateUniqueRandomNumber(bombList, 1, numberOfCells));
     }
+    // restituisco la lista delle bombe
     return bombList;
 }
 
-document.getElementById("button").addEventListener("click", function () {
-    createGame()
-})
+//$funzione pr generare un numero randomico non ripetuto
+function generateUniqueRandomNumber(numsBlacklist, min, max) {
+    // mi creo una variabile inizializzata a false, che mi controlla se ho generato un numero
+    // valido oppure no
+    let check = false;
+    let randomInt;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*                 creare griglie con for            */
-
-/* //creo una variabile e la prendo dalla griglia grande
-const gridElement = document.getElementById("grid");
-
-//arrow function che restituisce un div 
-const createSquare = () => {
-    const currentGrid = document.createElement("div");
-    currentGrid.classList.add("box");
-    return currentGrid;
-}
-// ogni volta che premo il bottone mi crea  le griglie
-document.getElementById('button').addEventListener('click', function () {
-    //resetto prima
-    gridElement.innerHTML = "";
-    //inizializzo il valore una variabile prendendolo dall'id html(select)
-    let grid = document.getElementById("input-level").value;
-    //se il select è uguale all'input che ho dato allora mi creo...
-    if (grid == "easy") {
-        for (i = 1; i <= 100; i++) {
-            //creo una variabile uguale alla funzione
-            const gridSquare = createSquare();
-            //aggiungo il numero alla griglia
-            gridSquare.innerHTML += [i];
-            //appena premo un quadrato della griglia gli do un colore diverso(alternando con troggle)
-            gridSquare.addEventListener('click', function () {
-                this.classList.toggle('box-clicked');
-            })
-            //aggiungo un figlio lla griglia grande
-            gridElement.appendChild(gridSquare);
-        }
-        //uguale ma meno i
-    } else if (grid == "medium") {
-        for (i = 1; i <= 81; i++) {
-            const gridSquare = createSquare();
-            gridSquare.innerHTML += [i];
-
-            gridSquare.addEventListener('click', function () {
-                this.classList.toggle('box-clicked');
-            })
-            gridElement.appendChild(gridSquare);
-
-        }
-    } else { //uguale ma meno i
-        for (i = 1; i <= 49; i++) {
-            const gridSquare = createSquare();
-            gridSquare.innerHTML += [i];
-
-            gridSquare.addEventListener('click', function () {
-                this.classList.toggle('box-clicked');
-            })
-            gridElement.appendChild(gridSquare);
-
+    // creo un ciclo che continua finché non ho trovato un numero valido (assente in blacklist)
+    while (!check) {
+        //  genero randomicamente un numero intero tra il min e il max passati come argomenti
+        randomInt = Math.floor(Math.random() * ((max + 1) - min) + min);
+        // se il numero non è presente nella blacklist allora
+        if (!numsBlacklist.includes(randomInt)) {
+            // informo il resto della funzione che il numero è stato trovato ed è valido
+            // ==> esco dal ciclo while
+            check = true;
         }
     }
-}); */
+
+    // restituisco il numero valido che ho trovato
+    return randomInt;
+}
+
+//$funzione che serve per far vedere tutte le bombe appena ne clicco una
+function checkAndAddClass(parentElementId, bombList, classToAdd) {
+    //variabile che prende i figli della griglia
+    const squares = document.getElementById(parentElementId).children;
+    // per ogni quadrato presente
+    for (let i = 0; i < squares.length; i++) {
+        // se è anche una bomba
+        if (bombList.includes(parseInt(squares[i].innerHTML))) {
+            // la faccio esplodere
+            squares[i].classList.add(classToAdd);
+        }
+    }
+}
+
+//$ Funzione che scrive in un elemento del dom preso da id "elementId" sovrascrivendo ciò che è presente
+function writeInElementById(elementId, stringToWrite) {
+    document.getElementById(elementId).innerHTML = stringToWrite;
+}
+
+//£ funzione per generare una nuova partita con all'interno altre funzioni
+function createNewGame() {
+    // Reset generale per il contenitore interno della grid
+    document.querySelector('#grid').innerHTML = "";
+
+    // domanda, il gioco è finito?
+    let isGameOver = false;
+
+    // prima di tutto recupero il livello selezionato dall'utente
+    const level = parseInt(document.getElementById('input-level').value);
+    console.log(level);
+
+    // numero di celle per riga e numero di celle totali
+    let cellsPerRow;
+    let cellsNumber;
+
+    let points = 0;
+    const NUMBER_OF_BOMBS = 16;
+
+    //  prendere il valore in base alla difficoltà
+    switch (level) {
+        case 1:
+            cellsNumber = 100;
+            // cellsPerRow = 10;
+            break;
+        case 2:
+            cellsNumber = 81;
+            // cellsPerRow = 9;
+            break;
+        case 3:
+            cellsNumber = 49;
+            // cellsPerRow = 7;
+            break;
+    }
+
+    //$  cellsPerRow = radice quadrata di cellsNumber
+    cellsPerRow = Math.sqrt(cellsNumber);
+
+    // genero la lista delle bombe casuali
+    const bombs = generateBombList(NUMBER_OF_BOMBS, cellsNumber);
+    console.log(bombs);
+
+    //  ciclo per tutti gli elementi che voglio creare
+    for (let i = 1; i <= cellsNumber; i++) {
+        //  prendo il parent
+        const cell = createSquare(i, cellsPerRow);
+
+        //  aggiungo un event listener per far colorare il bottone
+        cell.addEventListener('click', function () {
+
+            // se il gioco non è finito allora
+            if (!isGameOver) {
+                if (!bombs.includes(i)) {
+                    this.classList.add('box-clicked');
+
+                    //aggiungo un contatore
+                    points++;
+
+                    //£Funzione che scrive in un elemento del dom sovrascrivendo ciò che è presente
+                    writeInElementById('points', `Il tuo punteggio è: <h3>${points}</h3>`);
+                } else {
+                    this.classList.add('box-bomber');
+                    writeInElementById('points', `Mi dispiace, hai perso, il tuo punteggio è: <h3>${points}</h3>`);
+
+                    //£funzione per uscire tutte le bombe
+                    checkAndAddClass('grid', bombs, 'box-bomber');
+                    isGameOver = true;
+                }
+            }
+        });
+        // inserisco come elementi figli del parent
+        document.querySelector('#grid').appendChild(cell);
+    }
+}
+
+//£ evento per creare la griglia
+document.getElementById('button').addEventListener("click", function () {
+    createNewGame();
+});
